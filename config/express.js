108f25a44ac01604ff7fcd08aa527d3c46389053
Christopher James <68414004+Mindful-Developer@users.noreply.github.com>
2023-09-19 22:14:50 -0400

    Restructure for assignment specs

--- config/express.js
+++ config/express.js
@@ -1,26 +1,26 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-var indexRouter = require("./routes/index");
-var aboutRouter = require("./routes/about");
-var projectsRouter = require("./routes/projects");
-var servicesRouter = require("./routes/services");
-var contactRouter = require("./routes/contact");
+const indexRouter = require("../app/routes");
+const aboutRouter = require("../app/routes/about");
+const projectsRouter = require("../app/routes/projects");
+const servicesRouter = require("../app/routes/services");
+const contactRouter = require("../app/routes/contact");
 
-var app = express();
+const app = express();
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "../app/views"));
 app.set("view engine", "ejs");
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "../public")));
 
 app.use("/", indexRouter);
 app.use("/about", aboutRouter);
---
+++ config/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: [
+    "../config/express.js",
+    "../app/routes/*.js",
+    "../app/views/**/*.ejs",
+    "../public/**/*.{js,html}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: ["prettier-plugin-tailwindcss"],
+};
